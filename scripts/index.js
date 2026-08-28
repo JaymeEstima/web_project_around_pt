055bea33d0b53133066322197f1f5aadbbe48e3f
JaymeEstima <Jayme@Mac-mini.local>
2026-08-28 13:06:11 +0100

    Implementa modal de editar perfil com openModal/closeModal reutilizáveis

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,46 @@ const initialCards = [
 initialCards.forEach((card) => {
     console.log(card.name);
 });
+
+function openModal(modal){
+    modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal){
+    modal.classList.remove("popup_is-opened");
+}
+
+const editPopup = document.querySelector("#edit-popup");
+const editPopupCloseButton = editPopup.querySelector(".popup__close");
+const editForm = editPopup.querySelector("#edit-profile-form");
+const nameInput = editForm.querySelector(".popup__input_type_name");
+const descriptionInput = editForm.querySelector(".popup__input_type_description");
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const editButton = document.querySelector(".profile__edit-button");
+
+function fillProfileForm(){
+    nameInput.value = profileTitle.textContent;
+    descriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+    fillProfileForm();
+    openModal(editPopup);
+}
+
+editButton.addEventListener("click", handleOpenEditModal);
+
+editPopupCloseButton.addEventListener("click", function () {
+    closeModal(editPopup);
+});
+
+function handleProfileFormSubmit(event) {
+    event.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
+    closeModal(editPopup);
+}
+
+editForm.addEventListener("submit", handleProfileFormSubmit);
